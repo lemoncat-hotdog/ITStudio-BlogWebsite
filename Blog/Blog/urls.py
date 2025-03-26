d72faf858e8d21d53fb51b8f0a7817d2c5416ce9
Yaosanqi137 <zxc18023571263@outlook.com>
2025-03-26 09:55:51 +0800

    feat: created article app

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('user/', include('article.urls')),
     path('', include('pages.urls')),
     path('captcha/', include('captcha.urls')),
 ]
